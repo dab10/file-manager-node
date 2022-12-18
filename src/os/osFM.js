57518bbb4a80afe3f5c6e7f9aedebb1a863a5e7d
dab10 <dab10@list.ru>
2022-12-18 21:35:20 +0300

    fix: fix message in os block

--- src/os/osFM.js
+++ src/os/osFM.js
@@ -3,22 +3,23 @@ import os from 'node:os';
 export const osFM = async (query) => {
   const queryCommand = query.slice(3);
   if (queryCommand === '--EOL') {
-    process.stdout.write(JSON.stringify(os.EOL));
+    return process.stdout.write(JSON.stringify(os.EOL));
   }
   if (queryCommand === '--cpus') {
     const allData = os.cpus();
     process.stdout.write(`Total CPU: ${allData.length}\n`);
     allData.map(item => process.stdout.write(`model: ${item.model}\nspeed: ${item.speed / 1000} GHz\n`))
+    return;
   }
   if (queryCommand === '--homedir') {
-    process.stdout.write(os.homedir());
+    return process.stdout.write(os.homedir());
   }
   if (queryCommand === '--username') {
-    process.stdout.write(os.userInfo().username);
+    return process.stdout.write(os.userInfo().username);
   }
   if (queryCommand === '--architecture') {
-    process.stdout.write(os.arch());
+    return process.stdout.write(os.arch());
   } else {
-    process.stdout.write('Invalid input\n');
+    return process.stdout.write('Invalid input\n');
   }
 }
